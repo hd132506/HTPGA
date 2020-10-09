dd9ef8e7933f11bbcc88620d2f422390e83b42af
hd132506 <ch20792457@gmail.com>
2020-10-10 03:08:30 +0900

    Improved space class

--- classworktest.py
+++ classworktest.py
@@ -1,7 +1,18 @@
 from genes import *
 
-space = VerticesSpace((10, 4, 2))
-assert space.nVertices == 10 + 4 + 2
-space = VerticesSpace((1, 2, 2), [[2], [1, 3], [4, 5]])
+def VerticesSpaceTest():
+    space = VerticesSpace((10, 4, 2))
+    assert space.getnVertices() == 10 + 4 + 2
+    space = VerticesSpace((1, 2, 2), [[2], [1, 3], [4, 5]])
+    # space.set
 
+def HexagonTest():
+    # pos = 
+    # hex = Hexagon()
+    # hex.setPos([()])
+    # hex.rotate(space)
+    pass
+
+# p = Problem(n_hex=4)
+# sol_space = p.solve()
 print('Test Done!')
--- genes.py
+++ genes.py
@@ -1,21 +1,37 @@
+"""
+A class that represents chromosome.
+contactLevels: Triple that contains the number of vertices for each contact level
+vertices: List of 3 vectors
+nVertices: The number of whole vertices, sum(contactLevels)
+"""
 class VerticesSpace:
-    """ Parameter
-    contactLevels: Triple that contains the number of vertices for each contact level
-    vertices: List of 3 vectors
-    """
     def __init__(self, contactLevels, vertices=None):
-        self.nVertices = sum(contactLevels)
-        self.contactLevels = contactLevels
-        self.space = []
+        self.__nVertices = sum(contactLevels)
+        self.__contactLevels = contactLevels
+        self.__space = []
         if vertices is None:
             self.makeAdam()
         else:
-            self.space = vertices
+            self.__space = vertices
     
+    # Class for building initial space
     def makeAdam(self):
         # Vertices that contact with nothing
-        self.space.append([i for i in range(1, self.contactLevels[0]+1)])
-        # Vertices that contact with another vertex
-        self.space.append([i for i in range(self.contactLevels[0]+1, sum(self.contactLevels[:2])+1)])
-        # Vertices that contact with 2 vertices
-        self.space.append([i for i in range(sum(self.contactLevels[:2])+1, self.nVertices+1)])
+        self.__space.append([i for i in range(1, self.__contactLevels[0]+1)])
+        self.__space.append([i for i in range(self.__contactLevels[0]+1, sum(self.__contactLevels[:2])+1)])
+        self.__space.append([i for i in range(sum(self.__contactLevels[:2])+1, self.__nVertices+1)])
+
+    # Getter
+    def getnVertices(self):
+        return self.__nVertices
+    def getcontactLevels(self):
+        return self.__contactLevels
+    def getspace(self):
+        return self.__space
+
+"""
+A class which consists of 6 tuples, which can perform operations to control VerticeSpace.
+"""
+class Hexagon:
+    def __init__(self, pointerList):
+        pass
