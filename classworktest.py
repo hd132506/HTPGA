3cd6e9072ce6d50a7a06db960b8723986e6045b4
hd132506 <ch20792457@gmail.com>
2020-10-13 15:39:00 +0900

    Make Eve

--- classworktest.py
+++ classworktest.py
@@ -1,10 +1,32 @@
 from genes import *
 
+def assertEqual(entity, target):
+    try:
+        assert entity == target
+    except:
+        print(f'{entity} is different from{target}')
+        raise AssertionError
+
 def VerticesSpaceTest():
-    space = VerticesSpace((10, 4, 2))
-    assert space.getnVertices() == 10 + 4 + 2
+    ## Space construction test
+    space = VerticesSpace((5, 3, 2))
+    assertEqual(space.getnVertices(), sum((5, 3, 2)))
+    assertEqual(space.getspace(), [[1, 2, 3, 4, 5], [6, 7, 8], [9, 10]])
+    space = VerticesSpace((5, 3, 2), 'Eve')
+    assertEqual(space.getspace(), [list(reversed([1, 2, 3, 4, 5])), list(reversed([6, 7, 8])), list(reversed([9, 10]))])
+    print('Space has been successfully constructed.')
+
     space = VerticesSpace((1, 2, 2), [[2], [1, 3], [4, 5]])
-    # space.set
+    ## Set element test
+    space.setElement((1, 1), 2)
+    space.setElement((0, 0), 1)
+    assertEqual(space.getspace(), [[1], [1, 2], [4, 5]])
+
+    print('Element of a space can be set.')
+
+    ## Swap test
+    # space.swapElement((0,1), (0, 2))
+    # print(space.getspace())
 
 def HexagonTest():
     # pos = 
@@ -15,4 +37,7 @@ def HexagonTest():
 
 # p = Problem(n_hex=4)
 # sol_space = p.solve()
-print('Test Done!')
+
+
+VerticesSpaceTest()
+print('All Tests Done!')
--- genes.py
+++ genes.py
@@ -9,19 +9,29 @@ class VerticesSpace:
         self.__nVertices = sum(contactLevels)
         self.__contactLevels = contactLevels
         self.__space = []
-        if vertices is None:
+
+        if (vertices is None) or (vertices == 'Adam'):
             self.makeAdam()
+        elif vertices == 'Eve':
+            self.makeEve()
         else:
             self.__space = vertices
+        
+        
     
     # Class for building initial space 
     def makeAdam(self):
         # For each space seperated by contact level, fill with just simple sequential numbers
         # So that it can be an initial, seed chromosome(Adam)
-        self.__space.append([i for i in range(1, self.__contactLevels[0]+1)])
-        self.__space.append([i for i in range(self.__contactLevels[0]+1, sum(self.__contactLevels[:2])+1)])
-        self.__space.append([i for i in range(sum(self.__contactLevels[:2])+1, self.__nVertices+1)])
-
+        if self.__space == []:
+            
+            self.__space.append([i for i in range(1, self.__contactLevels[0]+1)])
+            self.__space.append([i for i in range(self.__contactLevels[0]+1, sum(self.__contactLevels[:2])+1)])
+            self.__space.append([i for i in range(sum(self.__contactLevels[:2])+1, self.__nVertices+1)])
+    def makeEve(self):
+        self.makeAdam()
+        for i in range(len(self.__space)):
+            self.__space[i].reverse()
     # Getter
     def getnVertices(self):
         return self.__nVertices
@@ -29,6 +39,10 @@ class VerticesSpace:
         return self.__contactLevels
     def getspace(self):
         return self.__space
+
+    # Setter / modifier
+    def setElement(self, pos, value):
+        self.__space[pos[0]][pos[1]] = value
 
 """
 A class which consists of 6 tuples, which can perform operations to control VerticeSpace.
