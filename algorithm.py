598e7ad43e1b29d09708080180501c98e793983b
hd132506 <ch20792457@gmail.com>
2020-12-05 02:58:28 +0900

    Add algorithm skeleton

---
+++ algorithm.py
@@ -0,0 +1,20 @@
+from genes import *
+
+class GeneticAlgorithm:
+    def __init__(self, length, population):
+        self.__length = length
+        self.__population = population
+        self.tortoise = [Tortoise(length) for _ in range(population)]
+        # Additional parameters
+
+    def mutate(self):
+        pass
+
+    def crossover(self):
+        pass
+
+    def select(self):
+        pass
+
+    def solve(self):
+        pass
